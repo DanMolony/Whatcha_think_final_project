8d7001073e8fdccbcb56ac826c0ab34410229192
Dan Molony <dmolony16@gmail.com>
2022-06-06 19:20:31 -0400

    started, email password and rest

--- whatcha_think_project/whatcha_think_project/settings.py
+++ whatcha_think_project/whatcha_think_project/settings.py
@@ -133,3 +133,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'Whatcha-Think-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('GmailUser')
+EMAIL_HOST_PASSWORD = os.environ.get('GmailPass')
