805ff9b33233fe97c0559e8047af6fabc286e1d2
Dan Molony <dmolony16@gmail.com>
2022-06-08 14:47:13 -0400

    Working on image object for film poster, will return

--- whatcha_think_project/whatcha_think_project/urls.py
+++ whatcha_think_project/whatcha_think_project/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('', include('review.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
