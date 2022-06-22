95bb2237171605080e05e722c096b0b7fa49448e
Dan Molony <dmolony16@gmail.com>
2022-06-22 16:59:29 -0400

    working on views and models for filmposter app

--- whatcha_think_project/film_poster/views.py
+++ whatcha_think_project/film_poster/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import FilmPoster
 # Create your views here.
+class FilmPostView(User,FilmPoster):
+    model = FilmPoster
+    
+    def upload(request):
+        if request.method == 'POST':
+            form = FilmPoster(request.POST)
+        if form.is_valid():
+            form.save
